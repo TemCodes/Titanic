b9f1b78d36316b86411ea3174c42cb137b31fd9e
TemCodes <33301256+TemCodes@users.noreply.github.com>
2018-01-05 00:10:31 -0800

    Add files via upload
    Added linear regression models for Age and Fare.
    Added ordinal categories for SibSp and Parch and included them in the overall model.

--- Titanic Model.R
+++ Titanic Model.R
@@ -19,21 +19,49 @@ nrow(titanic.full)
 table(titanic.full$Embarked)
 titanic.full[titanic.full$Embarked == '',"Embarked"] <- 'S'
 
-#There are a lot of ages missing.
+#Clean age missing data.
 table(is.na(titanic.full$Age))
-median(titanic.full$Age, na.rm =TRUE)
-age.median <- median(titanic.full$Age)
-titanic.full[is.na(titanic.full$Age),"Age"] <- age.median
+boxplot(titanic.full$Age)
+#We want to get rid of all the outliears.
+upper.whisker.age <- boxplot.stats(titanic.full$Age)$stats[5]
+outlier.filter <- titanic.full$Fare < upper.whisker.age
+titanic.full[outlier.filter,]
+age.equation = "Age ~ Pclass + Sex + Fare + SibSp + Parch + Embarked"
+age.model <- lm(
+  formula = age.equation,
+  data = titanic.full[outlier.filter,]
+)
+age.row<-titanic.full[
+  is.na(titanic.full$Age),
+  c("Pclass", "Sex", "Fare", "SibSp", "Parch", "Embarked")
+  ]
+age.predictions <-predict(age.model, newdata = age.row)
+titanic.full[is.na(titanic.full$Age),"Age"] <- age.predictions
 
-#Fare NA's
+#Cleaning fare missing data.
 table(is.na(titanic.full$Fare))
-fare.median <- median(titanic.full$Fare)
-titanic.full[is.na(titanic.full$Fare),"Fare"] <- fare.median
+boxplot(titanic.full$Fare)
+#We want to get rid of all the outliears.
+upper.whisker <- boxplot.stats(titanic.full$Fare)$stats[5]
+outlier.filter <- titanic.full$Fare < upper.whisker
+titanic.full[outlier.filter,]
+fare.equation = "Fare ~ Pclass + Sex + Age + SibSp + Parch + Embarked"
+fare.model <- lm(
+    formula = fare.equation,
+    data = titanic.full[outlier.filter,]
+)
+fare.row<-titanic.full[
+    is.na(titanic.full$Fare),
+    c("Pclass", "Sex", "Age", "SibSp", "Parch", "Embarked")
+]
+fare.predictions <-predict(fare.model, newdata = fare.row)
+titanic.full[is.na(titanic.full$Fare),"Fare"] <- fare.predictions
 
 #Categorical Casting
 titanic.full$Pclass<- as.factor(titanic.full$Pclass)
 titanic.full$Sex<-as.factor(titanic.full$Sex)
 titanic.full$Embarked<-as.factor(titanic.full$Embarked)
+titanic.full$Embarked<-as.ordered(titanic.full$SibSp)
 
 #Data is cleaned, now we can pass them back into train and test.
 titanic.train<-titanic.full[titanic.full$IsTrainSet==TRUE,]
@@ -43,12 +71,13 @@ dim(titanic.test)
 
 titanic.train$Survived<-as.factor(titanic.train$Survived)
 
-#Creating the model
-survived.equation<-("Survived ~ Pclass + Sex + Age + SibSp + Parch +Fare + Embarked")
+
+#Creating the model______________________________________________________________________________________________________________________________________________
+survived.equation<-("Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked")
 survived.formula<-as.formula(survived.equation)
 install.packages("randomForest")
 library(randomForest)
-titanic.model<-randomForest(formula = survived.formula, data=titanic.train, ntree= 500, mtry=3,nodesize=0.01*nrow(titanic.train),na.action=na.exclude)
+titanic.model<-randomForest(formula = survived.formula, data=titanic.train, ntree= 500, mtry=3,nodesize=0.01*nrow(titanic.train))
 features.euqtion<- "Survived ~ Pclass + Sex + Age + SibSp + Parch +Fare + Embarked"
 Survived <- predict(titanic.model, newdata = titanic.test)
 Survived
@@ -56,9 +85,6 @@ PassengerId<- titanic.test$PassengerId
 output.df<-as.data.frame(PassengerId)
 output.df$Survived <- Survived
 output.df
-
-output.df[is.na(output.df$Survived),"Survived"] <- 0
-
-
-
+table(output.df$Survived)
 write.csv(output.df, file = "kaggle_submission.csv", row.names = FALSE)
+
